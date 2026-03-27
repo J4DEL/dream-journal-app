71a107f1a9bae64e213a82fb6db44a45da54feda
Jade Huang <jade.huang1@ucalgary.ca>
2026-03-27 14:31:11 -0600

    Added textbox, save button, and save function

--- main.py
+++ main.py
@@ -5,10 +5,25 @@ import tkinter as tk
 window = tk.Tk()
 
 # 3. Give the window a title at the very top
-window.title("MADJS Dream Journal")
+window.title("Dream Journal")
 
 # 4. Set the starting size of the window (Width x Height)
 window.geometry("500x400")
+
+def save_dream():
+    dream_text = journal_input.get("1.0", tk.END)
+
+    print("Dream entry successful.")
+
+    with open("dream.txt", "a") as file:
+        file.write(dream_text + "\n---\n")
+
+#text box
+journal_input = tk.Text(window)
+journal_input.pack()
+#save button
+save_button = tk.Button(window, text="Save Dream", command=save_dream)
+save_button.pack()
 
 # 5. Tell the window to stay open in an infinite loop and wait for the user
 window.mainloop()
